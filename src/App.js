8254411960cc60f102cb40065132fda53a743efe
Farooq Ezhar <waghih.co@gmail.com>
2019-10-23 14:15:01 +0800

    Fetch realtime api with websocket

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ActionCable from 'actioncable'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { text: '' }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/notes/1")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({ text: result.text })
+      },
+      (error) => {
+        this.setState({ text: '' })
+      }
+    )
+    const cable = ActionCable.createConsumer('ws://localhost:3000/cable')
+    this.sub = cable.subscriptions.create('NotesChannel', {
+      received: this.handleReceiveNewText
+    })
+  }
+
+  handleReceiveNewText = ({ text }) => {
+    if (text !== this.state.text) {
+      this.setState({ text })
+    }
+  }
+  
+  handleChange = e => {
+    this.setState({ text: e.target.value })
+    this.sub.send({ text: e.target.value, id: 1 })
+  }
+
+  render() {
+    return (
+      <textarea
+        value={this.state.text}
+        onChange={this.handleChange}
+      />
+    )
+  }
 }
 
 export default App;
